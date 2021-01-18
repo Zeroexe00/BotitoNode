a5ecbc62e8522c7766980f2c08e97d77187bdd4b
Zeroexe00 <exe_6084@hotmail.com>
2021-01-17 22:09:29 -0300

    adding democratic votes

--- index.js
+++ index.js
@@ -4,17 +4,21 @@ const ytdl = require("discord-ytdl-core");
 const client = new Discord.Client();
 
 let gameVotes = []
+let result = []
+let resultMessage = ''
 let isVoting = false
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+
+//funcion para la reproduccion de videos
 async function connectVoiceChannel(msg, str = '') {
   try {
     let voice_Channel = await client.channels.fetch(msg.member.voice.channelID);
-    
+
     if (!voice_Channel) return msg.reply("Error: el canal de voz no existe!");
-    
+
     let voice_Connection = await voice_Channel.join();
     // await voice_Connection.play(await ytdl('hhttps://www.youtube.com/watch?v=IDgm3fXa8Kw'), { type: 'opus' });
     // let stream = ytdl("https://www.youtube.com/watch?v=IDgm3fXa8Kw", {
@@ -33,7 +37,7 @@ async function connectVoiceChannel(msg, str = '') {
       await repro.on('finish', () => {
         msg.guild.me.voice.channel.leave();
       })
-    } else if(msg.content === 'hablemos') {
+    } else if (msg.content === 'hablemos') {
       let repro = await voice_Connection.play('sound.mp3');
       await repro.on('finish', () => {
         msg.guild.me.voice.channel.leave();
@@ -45,48 +49,71 @@ async function connectVoiceChannel(msg, str = '') {
     msg.reply("Error: ya me rompiste wey! " + error)
   }
 }
+
+//funcion para la votacion
 function handleDemocraticElection(msg) {
   let vote = {
     name: msg.author.id,
-    gameName:''
+    gameName: ''
   }
-  if(msg.content === 'f') {
+  if (msg.content === 'f') {
     vote.gameName = 'Fornite'
-  }else if (msg.content === 'lol') {
+  } else if (msg.content === 'lol') {
     vote.gameName = 'League of Legend'
   } else if (msg.content === 'p' || msg.content.includes('fobia')) {
     vote.gameName = 'Phasmofobia'
-  }else {
+  } else {
     msg.reply('is that a vote? you need to vote for f,lol,p')
   }
   // console.log(msg)
-  if(!gameVotes.some((e)=> e.name === msg.author.id)){
+  // if (!gameVotes.some((e) => e.name === msg.author.id)) {
     gameVotes.push(vote)
-  }else {
-    msg.reply('ya votaste hijito!')
-  }
+  // } else {
+  //   msg.reply('ya votaste hijito!')
+  // }
+
+  result = gameVotes.reduce((acc, cur) => {
+    if (acc) {
+      let i = acc.findIndex(e => e.gameName === cur.gameName)
+      if (typeof i === 'number' && i !== -1) {
+        acc[i].numVotes = acc[i].numVotes + 1
+        return [...acc]
+      } else {
+        return [...acc, { gameName: cur.gameName, numVotes: 1 }]
+      }
+    }
+  }, [])
+  result = result.sort((a,b) => a.numVotes + b.numVotes)
+  resultMessage = `El resultado fue ${result[0].gameName} con ${result[0].numVotes}`
 }
 client.on('message', async msg => {
   // console.log(JSON.stringify(msg.author.bot,null,2),isVoting)
   // console.log(gameVotes)
-  if(isVoting && !msg.author.bot) {
+  if (isVoting && !msg.author.bot) {
     handleDemocraticElection(msg)
   }
-  
-  if(msg.author.bot) return
 
-  if(msg.content === '/quejugar') {
-    isVoting= true
-    setTimeout(()=> {
+  if (msg.author.bot) return
+  //votacion de juegos
+  if (msg.content === '/quejugar') {
+
+    isVoting = !isVoting
+
+    if (!isVoting) return msg.channel.send('Ya comenzo una votación...')
+
+    setTimeout(() => {
       isVoting = false;
-      msg.channel.send('los resultados son : ' + JSON.stringify(gameVotes,null,2))
+      msg.channel.send(resultMessage)
       gameVotes = []
-    },120000)
+    }, 60000)
+
     msg.channel.send('Empieza la votación... los leo pueden votar por f o lol o p');
   }
+  //help no hay help
   if (msg.content === '/help') {
     msg.channel.send('ayuda con que gil! uheee');
   }
+  //eleccion random
   if (msg.content === '/jugar') {
     let val = Math.random()
     if (val < 0.3) {
@@ -97,24 +124,33 @@ client.on('message', async msg => {
       msg.channel.send('L');
     }
   }
+
+  //respuesta a naa buu
   if (msg.content.includes('naaa') || msg.content.includes('buu') || msg.content.includes('Naa')) {
     msg.channel.send('ay que lataa!');
   }
+
+  //reproducir video
   if (msg.content.includes('/re ')) {
     let message = msg.content.split('/re ')
     await connectVoiceChannel(msg, message[1])
   }
 
+
+  //phsmofobia meme
   if (msg.content.includes('manifiestate') || msg.content.includes('estas aqui') || msg.content.includes('danos una se')) {
     msg.channel.send('HERE');
   }
 
+
+  //respuesta a hue prometiste
   if (msg.content.includes('hue') || msg.content.includes('prometiste')) {
-    let value = Math.random() 
+    let value = Math.random()
     value > 0.8 ? msg.reply('tu me lo prometiste viejo!') : value < 0.4 ? msg.reply('oh me salio wequereke') : msg.reply('uheeeeee!')
   }
-  
-  if(msg.content === 'hablemos') {
+
+  //chatear  voz con el bot
+  if (msg.content === 'hablemos') {
     connectVoiceChannel(msg)
   }
 });
